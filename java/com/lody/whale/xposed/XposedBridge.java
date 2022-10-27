ba937044c26e96c6e227325b6273db10143b8efa
jyy41781 <jyy41781@ly.com>
2022-10-27 11:12:57 +0800

    slot有时会有负值

--- java/com/lody/whale/xposed/XposedBridge.java
+++ java/com/lody/whale/xposed/XposedBridge.java
@@ -101,7 +101,7 @@ public final class XposedBridge {
             XposedHelpers.resolveStaticMethod(hookMethod);
             AdditionalHookInfo additionalInfo = new AdditionalHookInfo(callbacks);
             long slot = WhaleRuntime.hookMethodNative(hookMethod.getDeclaringClass(), hookMethod, additionalInfo);
-            if (slot <= 0) {
+            if (slot == 0) { //slot有时会有负值
                 throw new IllegalStateException("Failed to hook method: " + hookMethod);
             }
             synchronized (sHookedMethodSlotMap) {
