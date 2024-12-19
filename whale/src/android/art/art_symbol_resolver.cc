aedee02750d69f099ec9573d1efea0cab5c86d23
xiaojye <ixiaojye@163.com>
2024-12-19 18:54:44 +0800

    解决编译问题：decltype 替换 typeof; IsInteger32() | IsInteger64() 改为 IsInteger32() || IsInteger64()

--- whale/src/android/art/art_symbol_resolver.cc
+++ whale/src/android/art/art_symbol_resolver.cc
@@ -71,12 +71,12 @@ SYMBOL kArt_JniEnvExt_NewLocalRef = "_ZN3art9JNIEnvExt11NewLocalRefEPNS_6mirror6
 
 bool ArtSymbolResolver::Resolve(void *elf_image, s4 api_level) {
 #define FIND_SYMBOL(symbol, decl, ret)  \
-        if ((decl = reinterpret_cast<typeof(decl)>(WDynamicLibSymbol(elf_image, symbol))) == nullptr) {  \
+    if ((decl = reinterpret_cast<decltype(decl)>(WDynamicLibSymbol(elf_image, symbol))) == nullptr) {  \
         if (ret) {  \
             LOG(ERROR) << "Failed to resolve symbol : " << #symbol;  \
             return false;  \
-         } \
-        }
+        } \
+    }
     FIND_SYMBOL(kArt_GetMethodShorty, symbols_.Art_GetMethodShorty, false);
     if (symbols_.Art_GetMethodShorty == nullptr) {
         FIND_SYMBOL(kArt_GetMethodShorty_Legacy, symbols_.Art_GetMethodShorty, false);
